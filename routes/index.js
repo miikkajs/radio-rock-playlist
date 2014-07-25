b946d507558808dfc14800673046633a02009b00
unknown <miikka.salomaa@msa.geometrix.lan>
2014-07-25 15:48:55 +0300

    Calculate artists and songs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,67 @@
-module.exports = function (app) {
-    app.get('/', index);
+var request = require('request');
+var calculted = [];
+module.exports = function(app) {
+	app.get('/', index);
 };
 
-var index = function (req, res) {
-	
-    res.render('index', { title: 'Node Boilerplate' });
+var index = function(req, res) {
+	request.get("http://www.radiorock.fi/api/programdata/getlatest?", function(err, response, body) {
+		var json = JSON.parse(body);
+		parseArtists(json.result, function() {
+			res.send(calculted);
+		});
+
+
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+};
+
+var findArtist = function(arr, name) {
+	for(var i = 0 ; i < arr.length ; ++i) {
+		if(arr[i].artist === name) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+var parseArtists = function(json, cb) {
+	json.forEach(function(data) {
+		var artistIndex = findArtist(calculted, data.artist);
+		if (artistIndex < 0) {
+			calculted.push({
+				artist: data.artist,
+				count: 1,
+				songs:
+				[
+					{
+						song: data.song,
+						count: 1
+					}
+				]
+			});
+		} else {
+			var foundArtist = calculted[artistIndex]; 
+			foundArtist.count++;
+			var found = false;
+			for(var i = 0 ; i < foundArtist.songs.length ; ++i) {
+				if(foundArtist.songs[i].song == data.song) {
+					console.log("fasfsa", foundArtist.songs[i]);
+					foundArtist.songs[i].count++;
+					found = true;
+				}
+			}
+
+			if(!found) {
+				foundArtist.songs.push({
+					song: data.song,
+					count: 1
+				});
+			}
+
+		}
+
+	});
+	cb();
 };
